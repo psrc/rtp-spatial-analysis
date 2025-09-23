e7d83bfecf9594679962b6726430ac60583eecf6
stefancoe <coestefan@gmail.com>
2025-09-23 07:50:26 -0700

    Moving points_in_polygon function to utils.py

--- rtp_spatial_analysis/src/utils.py
+++ rtp_spatial_analysis/src/utils.py
@@ -11,3 +11,14 @@ def buffer_layer(layer_gdf, distance):
 def intersect_layers(df1, df2, how='intersection'):
     return df1.overlay(df2, how= how)
 
+def points_in_polygon(points_gdf, polygons_gdf, col_name, buffer=0):
+    """Check if a point intersects a polygon with an optional buffer, and add a boolean column."""
+    if buffer > 0:
+        buffered_points_gdf = points_gdf.copy()
+        buffered_points_gdf.geometry = buffered_points_gdf.geometry.buffer(buffer)
+        intersects = buffered_points_gdf.geometry.intersects(polygons_gdf.geometry.unary_union)
+    else:
+        intersects = points_gdf.geometry.intersects(polygons_gdf.geometry.unary_union)
+    points_gdf[col_name] = intersects
+    return points_gdf
+
