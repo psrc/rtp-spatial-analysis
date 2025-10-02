b4de3f926e2b0835e49f3075081b0c77379ec630
christopeak <cpeak@psrc.org>
2025-10-02 15:01:48 -0700

    reformat density_and_signals.run() to PEP 8 conventions

--- rtp_spatial_analysis/src/density_and_signals.py
+++ rtp_spatial_analysis/src/density_and_signals.py
@@ -26,19 +26,33 @@ def run(config):
     """
 
     try:
-
-        signals = utils.get_onedrive_layer(config, 'its_signals_path', 'its_signals')
-        # signals['ped_signal'] = signals['ped_signal'].fillna(0)
+        signals = utils.get_onedrive_layer(
+            config, 'its_signals_path', 'its_signals'
+        )
         signals['ped_signal'] = signals['ped_signal'].replace('Null', 'No')
-        ped_signals = signals.query("ped_signal == 'Yes'")
-        au_2050 = utils.get_onedrive_layer(config, 'activity_units_path', 'peope_and_jobs_2050')
-        au_2050['is_dense'] = au_2050['au_acre'].apply(lambda x: 'high density' if x > 25 else 'low density')
-        gdf_ped_signals_with_density = signals.sjoin(au_2050, how='inner', predicate='within')
-        
-        gdf = gdf_ped_signals_with_density.drop('OBJECTID', axis=1)
+
+        au_2050 = utils.get_onedrive_layer(
+            config, 'activity_units_path', 'peope_and_jobs_2050'
+        )
+        au_2050['is_dense'] = au_2050['au_acre'].apply(
+            lambda x: 'high density' if x > 25 else 'low density'
+        )
+
+        ped_signals_with_density = signals.sjoin(
+            au_2050, how='inner', predicate='within'
+        )
+
+        gdf = ped_signals_with_density.drop('OBJECTID', axis=1)
         x_tab = pd.crosstab(gdf['ped_signal'], gdf['is_dense'])
-        utils.export_csv(x_tab, config, 'density_and_signals.csv', index=True)
-        utils.export_layer(gdf, config, lyr_nm='accesible_ped_signals_in_dense_areas')
+
+        utils.export_csv(
+            x_tab, config, 'density_and_signals.csv', index=True
+        )
+
+        utils.export_layer(
+            gdf, config, lyr_nm='accessible_ped_signals_in_dense_areas'
+        )
+        
         print(f"Finished Density and Signals")
         
     except Exception as e:
