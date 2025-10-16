27eebb3ecbef426dd7b32f3e117c3ef450570c18
Joanne <71201494+Joanneylin@users.noreply.github.com>
2025-10-16 14:59:28 -0700

    #2 update numbers

--- rtp_spatial_analysis/src/transit_stop_intersections.py
+++ rtp_spatial_analysis/src/transit_stop_intersections.py
@@ -10,7 +10,7 @@ transit_supportive_density = {'local':7,
                               'hct':40,
                               'brt':15}
 
-def get_service_au(config, buffered_stops, col_suffix):
+def get_service_au(config, buffered_stops, buffer_name):
 
     gdf = utils.get_onedrive_layer(config, 'activity_units_path', 'peope_and_jobs_2050')
     gdf = gdf.to_crs(2285)
@@ -30,42 +30,108 @@ def get_service_au(config, buffered_stops, col_suffix):
 
         _list = gdf[sum_fields].sum().to_list()
         _list_without = [total_au[i] - _list[i] for i in range(len(_list))]
-        data[key + col_suffix] = _list +_list_without
+        data[key] = _list +_list_without
     
     df = pd.DataFrame.from_dict(data, orient='index', columns=['people with service', 'jobs with service', 'activity units with service',
                                                                'people w/o service', 'jobs w/o service', 'activity units w/o service'])
     df = df.rename_axis('Route Type')
+    df['Buffer'] = buffer_name
 
     return df
 
-def get_pop_service_efa(config, block, buffered_stops, col_suffix):
+
+def get_parcel_with_efa_pop(config):
+    # read population from parcel
+    parcel_columns = ["parcel_id", "population_2050", "geometry"]
+    gdf_parcel = utils.get_onedrive_layer(config, 'au_path', 'draft_parcel_data_rtp_2026')
+    # only keep parcels with population
+    gdf_parcel = gdf_parcel[gdf_parcel['population_2050']>0]
+    gdf_parcel = gdf_parcel[["parcel_id", "population_2050", "geometry"]]
+    
+    # Load tract layer from ElmerGeo
+    tract_columns = ["geoid20", "county_name", "tractce20", "geometry"]
+    eg_conn = psrcelmerpy.ElmerGeoConn()
+    tract = eg_conn.read_geolayer('TRACT2020', project_to_wgs84= False)
+    tract = tract[tract_columns]
+
+    # get list of all layers in file: gpd.list_layers(Path(user_path)/config['rtp_efa_path'])
+    # 2023 Equity Focused Areas
+    efa = pd.read_csv(config['user_onedrive']/ config['rtp_efa_path']/ "equity_focus_areas_2023.csv")
+    efa['geoid20'] = efa['GEOID20'].astype(str)
+    # filter columns: keep population percentage by efa type
+    efa_pct_cols = efa.columns[efa.columns.str.endswith('prct_est')]
+    efa_pop_cols = efa_pct_cols.str.replace('_prct_est', '_efa_pop', regex=False)
+    efa = efa[['geoid20'] + efa_pct_cols.tolist()].fillna(0).copy()
+    
+    # spatial join parcel with tract to get geoid
+    gdf_parcel_tract = gdf_parcel.sjoin(tract, how="left")
+    parcel_tract = gdf_parcel_tract.drop(columns=['geometry'])
+
+    # merge parcel_tract with efa: get percentage of population
+    parcel_tract_efa = parcel_tract.merge(efa, on='geoid20', how='inner')
+    # estimate population in each efa by multiplying percentage with total population in each parcel
+    parcel_tract_efa[efa_pop_cols] = parcel_tract_efa[efa_pct_cols].mul(parcel_tract_efa['population_2050'], axis=0)
+    parcel_tract_efa = parcel_tract_efa[['parcel_id', 'geoid20', 'county_name'] + efa_pop_cols.to_list()].copy()
+    # create catgorical type for county_name to keep all counties in the output table
+    parcel_tract_efa['county_name'] = parcel_tract_efa['county_name'].astype('category')
+    
+    # merge back with parcel to get geometry
+    gdf_parcel_efa = gdf_parcel.merge(parcel_tract_efa, on='parcel_id', how='inner')
+
+    return(gdf_parcel_efa)
+
+def get_pop_service_efa(config, parcel, buffered_stops, buffer_name):
 
     # list of efa column names
-    efa_cols = list(config['equity_geog_dict'].keys())
+    efa_pop_cols = parcel.columns[parcel.columns.str.endswith('efa_pop')]
+    # pop_cols = efa_pop_cols.str.replace('_efa_pop', '_pop', regex=False).to_list()
+    pct_cols = efa_pop_cols.str.replace('_efa_pop', '_pct', regex=False).to_list()
     # total population in each efa
-    efa_total_pop = block[efa_cols].sum().to_list()
+    efa_total_pop = parcel.groupby('county_name', observed=False)[efa_pop_cols].sum()
+    
+    # dictionary to hold dataframes for each transit type
     data = {}
-
-    # loop through transit types
+    # [loop through transit types] get population in each efa with service, without service, and total
     for key in transit_supportive_density.keys():
         
         # get buffered stops by transit type
         transit_by_type = buffered_stops[buffered_stops[key]>0]
-        gdf = gpd.clip(block, transit_by_type)
-
-        # total population in each efa with service
-        _list = gdf[efa_cols].sum().to_list()
+        gdf = gpd.clip(parcel, transit_by_type)
+        
+        df = gdf.drop(columns=['geometry'])
+        # total population in each efa with/without service
+        within = df.groupby('county_name', observed=False)[efa_pop_cols].sum().fillna(0)
+        without = efa_total_pop - within
         # percentage
-        _list_percent = [f"{_list[i] / efa_total_pop[i]:.2%}" for i in range(len(_list))]
-        data[key + col_suffix] = _list +_list_percent
-    
-    # create column names
-    total_cols = [i + " Total Pop" for i in list(config['equity_geog_dict'].values())]
-    percent_cols = [i + " % in Service" for i in list(config['equity_geog_dict'].values())]
-    df_cols = total_cols + percent_cols
+        within_percent = within/efa_total_pop
+        within_percent.columns = pct_cols
+        without_percent = without/efa_total_pop
+        without_percent.columns = pct_cols
+        ## total percent should all be 1
+        total_percent = efa_total_pop/efa_total_pop
+        total_percent.columns = pct_cols
 
-    df = pd.DataFrame.from_dict(data, orient='index', columns=df_cols)
-    df = df.rename_axis('Route Type')
+        within_all = pd.concat([within, within_percent], axis=1)
+        within_all['Area'] = "Inside Buffered TRS"
+        without_all = pd.concat([without, without_percent], axis=1)
+        without_all['Area'] = "Outside Buffered TRS"
+        total_all = pd.concat([efa_total_pop, total_percent], axis=1)
+        total_all['Area'] = "Total"
+
+        df = pd.concat([within_all, without_all, total_all])
+
+        data[key] = df
+
+    # create final dataframe from data dictionary
+    df = pd.concat(data.values(),
+                   keys=data.keys(),
+                   names=['Route Type']).reset_index()
+    df['Buffer'] = buffer_name
+    df = df[['county_name', 'Route Type', 'Buffer', 'Area'] + efa_pop_cols.tolist() + pct_cols]
+
+    # formatting
+    df[efa_pop_cols] = df[efa_pop_cols].round(1)
+    df[pct_cols] = df[pct_cols].map(lambda x: f'{x:.1%}')
 
     return df
     
@@ -81,41 +147,22 @@ def run(config):
     
     # 1. Intersection of transit stops and future density ----
     # get number of people and jobs that are in supportive densities with service and in those in supportive densities without service (Gap)
-    df1 = get_service_au(config, buf2_transit_stops_2050, '_half_mi')
-    df2 = get_service_au(config, buf4_transit_stops_2050, '_quarter_mi')
+    df1 = get_service_au(config, buf2_transit_stops_2050, 'half mile')
+    df2 = get_service_au(config, buf4_transit_stops_2050, 'quarter mile')
     df_service_dense = pd.concat([df1, df2])
 
     # save to output folder
     utils.export_csv(df_service_dense, config, "transit_stops_density_intersect.csv", index=True)
 
     # 2. Intersection of transit stops and Equity Focus Areas ----
-    # Load blocks layer from ElmerGeo
-    eg_conn = psrcelmerpy.ElmerGeoConn()
-    blocks = eg_conn.read_geolayer("block2020_nowater")
-    blocks = blocks.to_crs(2285) 
-
-    # get list of all layers in file: gpd.list_layers(Path(user_path)/config['rtp_efa_path'])
-    # 2023 Equity Focused Areas
-    efa_2023 = utils.get_onedrive_layer(config, 'rtp_efa_path', 'overall')
-    efa_2023 = pd.DataFrame(efa_2023.drop(columns=['Shape_Length', 'Shape_Area', 'geometry']))
-    efa_2023.loc[:,'tractce20'] =  efa_2023['L0ElmerGeo_DBO_tract2020_nowater_geoid20'].str[-6:]
-
-    # get block-level population with EFA assignments
-    block_efa_2023 = blocks[['geoid20', 'county_name', 'tractce20', 'total_pop20', 'geometry']].\
-        merge(efa_2023, on='tractce20')
-    block_efa_2023['total_pop20'] = block_efa_2023['total_pop20'].astype('float')
-    block_efa_2023 = block_efa_2023[block_efa_2023['total_pop20']>0].copy()
-
-    # For each equity column, replace values > 0 with the block population from 'total_pop20', else 0
-    efa_cols = list(config['equity_geog_dict'].keys())
-    block_efa_2023[efa_cols] = block_efa_2023[efa_cols].gt(0).mul(block_efa_2023['total_pop20'], axis=0)
+    gdf_parcel_efa = get_parcel_with_efa_pop(config)
         
-    df1 = get_pop_service_efa(config, block_efa_2023, buf2_transit_stops_2050, '_half_mi')
-    df2 = get_pop_service_efa(config, block_efa_2023, buf4_transit_stops_2050, '_quarter_mi')
+    df1 = get_pop_service_efa(config, gdf_parcel_efa, buf2_transit_stops_2050, 'half mile')
+    df2 = get_pop_service_efa(config, gdf_parcel_efa, buf4_transit_stops_2050, 'quarter mile')
     df_pop_service = pd.concat([df1, df2])
 
     # save to output folder
-    utils.export_csv(df_pop_service, config, "transit_stops_efa_pop_intersect.csv", index=True)
+    utils.export_csv(df_pop_service, config, "transit_stops_efa_pop_intersect.csv")
     
     print ('done')
 
