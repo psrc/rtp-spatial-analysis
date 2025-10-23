a45978b68dcf397c03bb6d6b50720f5ef8d5e002
Kris Overby <KOverby@psrc.org>
2025-10-23 12:38:55 -0700

    generic commit message

--- rtp_spatial_analysis/src/congestion_measures.py
+++ rtp_spatial_analysis/src/congestion_measures.py
@@ -4,41 +4,35 @@ import pandas as pd
 import utils
 
 # Heavy or Severe Congestion and FGTS routes
-# Function to identify congested links with fgts > 0 and export to shapefile and gdb
-
-def congested_fgts(congested_links_fgts, network_gdf):
-    # select links from network gdf that are in congested links and have @fgts > 0
-    congested_links_fgts = network_gdf[network_gdf['ID'].isin(congested_links_fgts['ij'])]
-    congested_links_fgts = congested_links_fgts[congested_links_fgts['@fgts'] > 0]
-    congested_links_fgts = congested_links_fgts[['ID', '@fgts', 'geometry']]
-    congested_links_fgts = congested_links_fgts.sort_values(by='@fgts', ascending=False)
-    return congested_links_fgts
-
-   
-
+  
 # Frequent Transit Routes and Heavy or Severe Congestion
-#
-#def congested_transeg(congested_links, freqtran_gdf):
-    # select frequent routes from transit gdf that are in congested links
-#    congested_links = network_gdf[network_gdf['ID'].isin(congested_links['ij'])]
-#    congested_freqroutes = transit_gdf[transit_gdf['link_id'].isin(congested_links['ij'])]
-#    congested_freqroutes = congested_freqroutes[['route_id', 'link_id', 'geometry']]
-#    congested_freqroutes = congested_freqroutes.sort_values(by='route_id')
-#    congested_freqroutes.to_file("congested_freqtran.shp")
 
-    #export to file gdb
-#    congested_freqroutes.to_file("congested_freqtran.gdb", driver="OpenFileGDB")
+def congested_transit_segments(congested_links, transit_segments, transit_routes):   
+    congested_transit_segments=transit_segments[transit_segments['ij'].isin(congested_links['ij'])]
+    congested_transit_lines=congested_transit_segments.line_id.unique().tolist()
+    congested_transit_routes=transit_routes[transit_routes['ID'].isin(congested_transit_lines)]
+    return congested_transit_routes
+
 
 def run(config):
     model_links = pd.read_csv(Path(config['2050_model_run_path'])/"outputs/network/network_results.csv")
-    model_links = model_links[model_links['congestion_category'].isin(['Heavy', 'Severe'])]
-    network_gdf = gpd.read_file(Path(config['2050_model_run_path'])/"outputs/network/shapefile/emme_links.shp")
-    congested_links=congested_fgts(model_links, network_gdf)
-    #congested_links.to_file(Path(f"{config["rtp_output_path"]}/future_system_output.gdb"), layer="congested_fgts", driver="OpenFileGDB")
-    utils.export_layer(congested_links, config, "congested_fgts.shp")
+    model_links_gdf = gpd.read_file(Path(config['2050_model_run_path'])/"outputs/network/shapefile/emme_links.shp")    
+    #model_links["ij"] = model_links["i_node"].astype(str) + "-" + model_links["j_node"].astype(str)
+    congested_links = model_links[model_links['congestion_category'].isin(['Heavy', 'Severe'])]
+    congested_model_links_gdf = model_links_gdf[model_links_gdf['ID'].isin(congested_links['ij'])]
+    congested_fgts = congested_links[congested_links['@fgts'] > 0]
+    congested_fgts = model_links_gdf[model_links_gdf['ID'].isin(congested_fgts['ij'])]
+    utils.export_layer(congested_fgts, config, "congested_fgts.shp")
 
+    model_transit_segments = pd.read_csv(Path(config['2050_model_run_path'])/"outputs/transit/transit_segment_results.csv", dtype={'i_node': 'Int64', 'j_node': 'Int64'})
+    model_transit_segments["i_node"].fillna(0).astype(int)
+    model_transit_segments["j_node"].fillna(0).astype(int)
+    model_transit_segments["ij"] = model_transit_segments["i_node"].astype(str) + "-" + model_transit_segments["j_node"].astype(str)
+    
+    model_transit_routes = gpd.read_file(Path(config['2050_model_run_path'])/"outputs/network/shapefile/emme_tlines.shp")
+    congested_transit_routes = congested_transit_segments(congested_links, model_transit_segments, model_transit_routes) 
+    utils.export_layer(congested_model_links_gdf, config, "congested_links.shp")
+  
 
- #   congested_transeg(model_links, gpd.read_file(config['frequent_transit_routes_shapefile']))
-   
     print('done')
 
